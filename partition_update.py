823fc15a3978170e4db4a63a537bc79386fb94a1
河伯 <zrhebo@gmail.com>
2017-08-21 18:28:29 +0800

    check hive execute

--- partition_update.py
+++ partition_update.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
 
+import re
 from logger import logger_etl as logger
 from shell_client import LocalHiveExecutor
 
 
-def _real_main():
+def update_hive_table(tbl_name):
     logger.debug('hello')
     stmt = 'show tables from source'
     lhe = LocalHiveExecutor(stmt)
     rows = lhe.execute()
-    logger.debug(rows)
+
+    pattern = tbl_name + r'\d*$'
+
+    logger.debug(pattern)
+    releated_tbl_names = [tn for tn in rows if re.search(pattern, tn)]
+    logger.debug(releated_tbl_names)
+    pass
+
+
+def _real_main():
+    tbl_name = 'product_ext'
+    logger.debug(tbl_name)
+    update_hive_table(tbl_name)
     pass
 
 
