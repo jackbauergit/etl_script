43d1d0badb5cdb5c5667d692d8f38aa166cd06b1
河伯 <zrhebo@gmail.com>
2017-08-21 21:31:14 +0800

    check hive execute

--- partition_update.py
+++ partition_update.py
@@ -8,6 +8,7 @@ from config import src_db_name, dest_db_name, test_mode
 
 
 update_by_col = 'modified'
+partition_col = 'created'
 
 
 def update_hive_table(tbl_name, begin_date):
@@ -29,7 +30,7 @@ def _splice_table_cols(tbl_name):
         if not val:
             continue
 
-        if val == update_by_col:
+        if val == partition_col:
             continue
 
         cols.append(val)
@@ -39,22 +40,46 @@ def _splice_table_cols(tbl_name):
 
 def _update_hive(
         src_tbl_name, dest_tbl_name, update_cols, begin_date, gap_days=1):
-    end_date = begin_date + datetime.timedelta(days=gap_days)
     begin_date_str = begin_date.strftime('%Y-%m-%d %H:%M:%S')
-    end_date_str = end_date.strftime('%Y-%m-%d %H:%M:%S')
+    end_date_str = _get_after_day(begin_date, gap_days)
     update_stmt = (
         "INSERT OVERWRITE TABLE %s.%s PARTITION (dt) "
         "SELECT %s, substring(%s, 0, 10) as dt from %s.%s "
         "WHERE %s>='%s' and %s<='%s'") % (
-            dest_db_name, dest_tbl_name, update_cols, update_by_col,
-            src_db_name, src_tbl_name, update_by_col, begin_date_str,
-            update_by_col, end_date_str)
+            dest_db_name, dest_tbl_name, update_cols, partition_col,
+            src_db_name, src_tbl_name, partition_col, begin_date_str,
+            partition_col, end_date_str)
     lhe = LocalHiveExecutor(update_stmt)
     lhe.execute()
     pass
 
 
+def _get_after_day(curr_date, gap_days=1):
+    after_date = curr_date + datetime.timedelta(days=gap_days)
+    return after_date.strftime('%Y-%m-%d %H:%M:%S')
+
+
+def _load_updated_src_tbl_names(tbl_name, begin_date):
+    begin_date_str = begin_date.strftime('%Y-%m-%d %H:%M:%S')
+    end_date_str = _get_after_day(begin_date)
+    tbl_names = _load_src_tbl_names(tbl_name)
+    updated_src_tbls = list()
+    for stn in tbl_names:
+        query_stmt = (
+            "SELECT DISTINCT 1 FROM %s.%s WHERE %s>='%s' and %s<='%s'") % (
+                src_db_name, tbl_name, update_by_col, begin_date_str,
+                update_by_col, end_date_str)
+        lhe = LocalHiveExecutor(query_stmt)
+        rows = lhe.execute()
+        logger.debug(rows)
+        if rows:
+            updated_src_tbls.append(stn)
+
+    return updated_src_tbls
+
+
 def _load_src_tbl_names(tbl_name):
+    logger.debug(tbl_name)
     if test_mode:
         return ['product_ext_with_no_partition']
 
@@ -66,7 +91,6 @@ def _load_src_tbl_names(tbl_name):
 
     logger.debug(pattern)
     releated_tbl_names = [tn for tn in rows if re.search(pattern, tn)]
-    logger.debug(tbl_name)
     logger.debug(releated_tbl_names)
     return releated_tbl_names
 
