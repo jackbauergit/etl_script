33f30022c7791b1b2086412b42901c6be37dc922
河伯 <zrhebo@gmail.com>
2017-08-22 10:33:59 +0800

    check hive execute

---
+++ update_partition.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+
+import re
+import datetime
+from logger import logger_etl as logger
+from shell_client import LocalHiveExecutor
+from config import src_db_name, dest_db_name, test_mode
+
+
+update_by_col = 'modified'
+partition_col = 'created'
+partition_name = 'dt'
+
+
+def update_hive_table(tbl_name, begin_date):
+    logger.debug('begin update')
+    dest_tbl_name = '%s_with_1_partition' % tbl_name
+    update_cols = _splice_table_cols('%s.%s' % (dest_db_name, dest_tbl_name))
+    update_src_tbl_names = _load_updated_src_tbl_info(tbl_name, begin_date)
+
+    _clean_expired_partition(update_src_tbl_names)
+    for stn, pts in update_src_tbl_names.iteritems():
+        _update_hive(
+            stn, dest_tbl_name, update_cols, pts)
+
+
+def _clean_expired_partition(partitions_in_tbl):
+    partition_collector = dict()
+    for _, pts in partitions_in_tbl.iteritems():
+        for pt in pts:
+            partition_collector[pt] = 1
+
+    clean_pts = partition_collector.keys()
+    for pt in clean_pts:
+        clean_stmt = "drop partition '%s'" % pt
+        lhe = LocalHiveExecutor(clean_stmt)
+        lhe.execute()
+
+
+def _splice_table_cols(tbl_name):
+    query_stmt = "SHOW COLUMNS FROM %s" % tbl_name
+    lhe = LocalHiveExecutor(query_stmt)
+    rows = lhe.execute()
+    cols = list()
+    tbl_filter = [partition_col, partition_name]
+    for val in rows:
+        if not val:
+            continue
+
+        if val in tbl_filter:
+            continue
+
+        cols.append(val)
+
+    return ', '.join(cols)
+
+
+def _load_updated_src_tbl_info(tbl_name, begin_date):
+    begin_date_str = begin_date.strftime('%Y-%m-%d %H:%M:%S')
+    end_date_str = _get_after_day(begin_date)
+    src_tbl_names = _load_src_tbl_names(tbl_name)
+    need_update_partitions = dict()
+    for stn in src_tbl_names:
+        query_stmt = (
+            "SELECT DISTINCT substring(%s, 0, 10) FROM %s.%s "
+            "WHERE %s>='%s' and %s<='%s'") % (
+                partition_col, src_db_name, stn, update_by_col,
+                begin_date_str,
+                update_by_col, end_date_str)
+        lhe = LocalHiveExecutor(query_stmt)
+        rows = lhe.execute()
+        logger.debug(rows)
+
+        if rows:
+            partitions = need_update_partitions.get(stn, {})
+            for pt in rows:
+                partitions[pt] = 1
+
+            need_update_partitions[stn] = partitions
+
+    return need_update_partitions
+
+
+def _load_src_tbl_names(tbl_name):
+    logger.debug(tbl_name)
+    if test_mode:
+        return ['product_ext_with_no_partition']
+
+    stmt = 'show tables from %s' % dest_db_name
+    lhe = LocalHiveExecutor(stmt)
+    rows = lhe.execute()
+
+    pattern = tbl_name + r'(_\d+)?$'
+
+    logger.debug(pattern)
+    releated_tbl_names = [tn for tn in rows if re.search(pattern, tn)]
+    logger.debug(releated_tbl_names)
+    return releated_tbl_names
+
+
+def _update_hive(
+        src_tbl_name, dest_tbl_name, update_cols, partitions, gap_days=1):
+    #  begin_date_str = begin_date.strftime('%Y-%m-%d %H:%M:%S')
+    #  end_date_str = _get_after_day(begin_date, gap_days)
+    pts = ["'%s'" % pt for pt in partitions]
+    pt_str = ', '.join(pts)
+    update_stmt = (
+        "INSERT OVERWRITE TABLE %s.%s PARTITION (%s) "
+        "SELECT %s, substring(%s, 0, 10) as %s from %s.%s "
+        "WHERE %s IN (%s)") % (
+            dest_db_name, dest_tbl_name, partition_name, update_cols,
+            partition_col, partition_name, src_db_name, src_tbl_name,
+            partition_col, pt_str)
+    lhe = LocalHiveExecutor(update_stmt)
+    lhe.execute()
+    pass
+
+
+def _get_after_day(curr_date, gap_days=1):
+    after_date = curr_date + datetime.timedelta(days=gap_days)
+    return after_date.strftime('%Y-%m-%d %H:%M:%S')
+
+
+def _real_main():
+    tbl_name = 'product_ext'
+    update_date = datetime.datetime.strptime('2014-12-05', '%Y-%m-%d')
+    update_hive_table(tbl_name, update_date)
+    pass
+
+
+if __name__ == '__main__':
+    _real_main()
